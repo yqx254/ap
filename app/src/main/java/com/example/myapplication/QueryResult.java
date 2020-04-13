a66ed33e28274fb6a72a4e462f42637c484f9048
yqx254 <ravix254@outlook.com>
2020-04-13 22:15:10 +0800

    failed tries

--- app/src/main/java/com/example/myapplication/QueryResult.java
+++ app/src/main/java/com/example/myapplication/QueryResult.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +17,8 @@ public class QueryResult extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar myChildToolbar = findViewById(R.id.toolbar2);
-        setSupportActionBar(myChildToolbar);
+//        Toolbar myChildToolbar = findViewById(R.id.toolbar2);
+//        setSupportActionBar(myChildToolbar);
         setContentView(R.layout.query_result);
         Intent intent = getIntent();
         String s = intent.getStringExtra(MainActivity.QUERY);
@@ -24,7 +27,13 @@ public class QueryResult extends AppCompatActivity{
         listView = findViewById(R.id.listView2);
         listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,mStrings));
         listView.setTextFilterEnabled(true);
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(QueryResult.this,"num" + id,Toast.LENGTH_LONG).show();
+            }
+        });
+//        ActionBar ab = getSupportActionBar();
+//        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
